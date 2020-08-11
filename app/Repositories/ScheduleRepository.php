a828c85de98a3d1f1c317f3543c639e11be46e57
Dominic Enriquez <dominic.enriquez@smartchartsfx.com>
2020-08-11 16:13:28 +0800

    connect schedule.coach_id = coach.id

--- app/Repositories/ScheduleRepository.php
+++ app/Repositories/ScheduleRepository.php
@@ -8,9 +8,9 @@ use learntotrade\salesforce\fields\CoachingSessionFields;
 use learntotrade\salesforce\Person;
 use learntotrade\salesforce\fields\PersonFields;
 use Carbon\Carbon;
-use learntotrade\salesforce\Sale;
-use learntotrade\salesforce\fields\SaleFields;
 use Illuminate\Support\Arr;
+use App\Repositories\Interfaces\CoachRepositoryInterface;
+use App\Repositories\Interfaces\SaleRepositoryInterface;
 
 class ScheduleRepository implements ScheduleRepositoryInterface
 {
@@ -32,18 +32,21 @@ class ScheduleRepository implements ScheduleRepositoryInterface
     {
         $data = [];
 
+        $coach = resolve(CoachRepositoryInterface::class)->all();
+        $coaches = Arr::pluck($coach['coaches'], 'country_code', 'id');
+
         $where = 'Date__c >= '.date('Y-m-d');
 
         if (auth()->guard('portal')->check()) {
             $salesforceToken = auth()->guard('portal')->user()->salesforce_token;
             
             $person = resolve(Person::class)->get($salesforceToken);
+
+            $sale = resolve(SaleRepositoryInterface::class)->all();
+            $saleId = Arr::pluck($sale['sales'], 'id');
             
-            $sale = resolve(Sale::class)->query(['Id'], [SaleFields::CUSTOMER.' = \''.$salesforceToken.'\'']);
-            $saleId = Arr::pluck($sale['records'], 'Id');
-            
-            $where .= ' and (' . CoachingSessionFields::SALE . ' IN (\'' . implode('\',\'', $saleId) . '\') or ' . 
-                      CoachingSessionFields::STATUS . ' = \'Pending\')';
+            $where = CoachingSessionFields::SALE . ' IN (\'' . implode('\',\'', $saleId) . '\') or ' . 
+                     CoachingSessionFields::STATUS . ' = \'Pending\'';
         }
 
         $sf = resolve(CoachingSession::class)->query(
@@ -56,21 +59,27 @@ class ScheduleRepository implements ScheduleRepositoryInterface
                 foreach (config('api.sf_schedule') as $key => $val) {
                     $data[$field][$key] = $value[$val];
                 }
-            
-                $start = Carbon::createFromFormat('Y-m-d h:i', $data[$field]['date'] . ' ' . $data[$field]['start_time'], 'UTC');
-                $end = Carbon::createFromFormat('Y-m-d h:i', $data[$field]['date'] . ' ' . $data[$field]['end_time'], 'UTC');
 
-                if (isset($person)) {
-                    $start->setTimezone($person[PersonFields::TIMEZONE]);
-                    $end->setTimezone($person[PersonFields::TIMEZONE]);
-                }
+                if (isset($coaches[$data[$field]['coach_id']])) {
+                    $coachTimezone = \DateTimeZone::listIdentifiers(\DateTimeZone::PER_COUNTRY, $coaches[$data[$field]['coach_id']])[0];
+                    $start = Carbon::createFromFormat('Y-m-d h:i', $data[$field]['date'] . ' ' . $data[$field]['start_time'], $coachTimezone);
+                    $end = Carbon::createFromFormat('Y-m-d h:i', $data[$field]['date'] . ' ' . $data[$field]['end_time'], $coachTimezone);
 
-                $start = explode(' ', $start->format('Y-m-d h:i'));
-                $end = explode(' ', $end->format('Y-m-d h:i'));
+                    if (isset($person)) {
+                        $start->setTimezone($person[PersonFields::TIMEZONE]);
+                        $end->setTimezone($person[PersonFields::TIMEZONE]);
+                    }
+
+                    $start = explode(' ', $start->format('Y-m-d h:i'));
+                    $end = explode(' ', $end->format('Y-m-d h:i'));
+                    
+                    $data[$field]['converted_date'] = $start[0];
+                    $data[$field]['converted_start_time'] = $start[1];
+                    $data[$field]['converted_end_time'] = $end[1];
                 
-                $data[$field]['converted_date'] = $start[0];
-                $data[$field]['converted_start_time'] = $start[1];
-                $data[$field]['converted_end_time'] = $end[1];
+                } else {
+                    unset($data[$field]);
+                }
             }
         }
 
