b9710065da306d0c673075f10befcd3f84444488
Dominic Enriquez <dominic.enriquez@smartchartsfx.com>
2021-10-26 11:00:34 +0800

    cancellation feature on coaching session.

--- app/Http/Controllers/Coaching/V1/CoachingSessionController.php
+++ app/Http/Controllers/Coaching/V1/CoachingSessionController.php
@@ -9,6 +9,8 @@ use learntotrade\salesforce\CoachingSession;
 use learntotrade\salesforce\fields\CoachingSessionFields;
 use App\Repositories\Interfaces\SaleRepositoryInterface;
 use App\Repositories\Interfaces\ScheduleRepositoryInterface;
+use learntotrade\salesforce\Exceptions\SalesforceException;
+use Carbon\Carbon;
 
 class CoachingSessionController extends Controller
 {
@@ -29,38 +31,79 @@ class CoachingSessionController extends Controller
 
         $result = [
             'status' => 'error',
-            'message' => 'Booking Error.'
+            'message' => 'Unable to book.'
         ];
 
         if ($computedCredits['total_available'] > 0) {
 
-            $sessionsRemaining = collect($sfSales['sales'])->sortBy('date')->where('sessions_remaining', '>', 0)->first();
+            $sessionsRemaining = collect($sfSales['sales'])->sortBy('date')
+                                                           ->where('sessions_remaining', '>', 0)
+                                                           ->first();
 
-            if (resolve(CoachingSession::class)->update($request->schedule_id, [
-                CoachingSessionFields::STATUS => 'Booked',
-                CoachingSessionFields::SALE => $sessionsRemaining['id'],
-            ])) {
+            try {
+                resolve(CoachingSession::class)->update($request->schedule_id, [
+                    CoachingSessionFields::STATUS => 'Booked',
+                    CoachingSessionFields::SALE => $sessionsRemaining['id'],
+                ]);
                 $result = [
                     'status' => 'success',
                     'message' => 'Successfully Booked.'
                 ];
+                
+            } catch (SalesforceException $e) {
+                $result['message'] = parent::catchSalesforceException($e);
             }
 
         } else {
-            $result = [
-                'status' => 'error',
-                'message' => 'Insufficient Credit.',
-            ];
+            $result['message'] = 'Insufficient Credit.';
         }
 
-        $result['computed_credits'] = $this->saleRepository->computedCredits($this->saleRepository->all()['sales']);
-        $result['schedules'] = $this->scheduleRepository->all()['schedules'];
-
-        return new CoachingSessionResource(collect($result));
+        return new CoachingSessionResource(
+            collect($result)->merge($this->refreshComputedCredits())
+        );
     }
 
     public function cancel(CoachingSessionRequest $request)
     {
-        return new CoachingSessionResource(collect(['cancel']));
+        $result = ['status' => 'error'];
+
+        $session = resolve(CoachingSession::class)->get($request->schedule_id);
+        
+        if (
+            empty($session)
+            || $session[CoachingSessionFields::STATUS] !== 'Booked'
+            || Carbon::createFromFormat(
+                'Y-m-d H:i',
+                $session[CoachingSessionFields::DATE] . ' ' . $session[CoachingSessionFields::START_TIME]
+            )->isPast()
+        ) {
+            $result['message'] = 'Unable to cancel.';
+
+        } else {
+            try {
+                resolve(CoachingSession::class)->update($request->schedule_id, [
+                    CoachingSessionFields::STATUS => 'Cancelled',
+                ]);
+                $result = [
+                    'status' => 'success',
+                    'message' => 'Successfully Cancelled.'
+                ];
+                
+            } catch (SalesforceException $e) {
+                $result['message'] = parent::catchSalesforceException($e);
+            }
+        }
+
+        return new CoachingSessionResource(
+                    collect($result)->merge($this->refreshComputedCredits())
+                );
+    }
+
+    private function refreshComputedCredits()
+    {
+        return [
+            'computed_credits' => $this->saleRepository->computedCredits($this->saleRepository->all()['sales']),
+            // 'schedules' => $this->scheduleRepository->all()['schedules'],
+        ];
     }
 }
