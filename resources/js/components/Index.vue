a86e12231fffd7a7296913e42d8001360cd8f135
jan_israel <jan.israel@smartchartsfx.com>
2021-11-13 16:03:12 +0800

    update, mounted

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -502,12 +502,11 @@ export default {
     }
   },
   mounted() {
-
-    console.log(this.$el.textContent) // I'm text inside the component.
+console.log('mounted')
     this.coach_token = document.getElementById("ctrMain").src
     // https://dev-coaching.smartchartsfx.com/session/token/dcxXCVvu0qMTvvBaoUOia1Y5rHXg6cijqzmRpdvZWULxOEvQvXzq1xpyyAZDLg8fjH4ckd7KNfdF7gDeu09myv08ugrETO8RWTrJ#wid=17d17b22c45cdaf14efff705c9ae000e&title=Widget
     const str = this.coach_token
-    const slug = str.split('token/').pop()
+    const slug = str.split('v1?pl=').pop()
     let str_res = slug.replaceAll('&title=Widget', '')
     this.coach_token = str_res
     this.read()
