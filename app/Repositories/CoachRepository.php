025109ad0ef3f283b29966717db611b7dcea59ce
Dominic Enriquez <dominic.enriquez@smartchartsfx.com>
2021-11-17 22:08:04 +0800

    updates

--- app/Repositories/CoachRepository.php
+++ app/Repositories/CoachRepository.php
@@ -53,6 +53,9 @@ class CoachRepository implements CoachRepositoryInterface
         $options = [];
         $person = resolve(Person::class)->get(session('portal_user')->salesforce_token);
         $customerGroup = $this->customer_group[$person[PersonFields::CUSTOMER_GROUP]] ?? $this->customer_group['SC2'];
+        if ($customerGroup == 'LTT Legacy') {
+            $customerGroup = $customerGroup['LTT'];
+        }
 
         $customerType = [
             'Front End' => UserFields::CAN_COACH_FRONT_END.' = true',
