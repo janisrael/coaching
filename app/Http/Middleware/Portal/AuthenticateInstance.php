3db16a9f196033feebeac5cc37a34ac3c21fa551
Dominic Enriquez <dominic.enriquez@smartchartsfx.com>
2022-01-22 17:23:39 +0800

    fixed condition on portal instance

--- app/Http/Middleware/Portal/AuthenticateInstance.php
+++ app/Http/Middleware/Portal/AuthenticateInstance.php
@@ -16,21 +16,19 @@ class AuthenticateInstance
      */
     public function handle($request, Closure $next)
     {
-        $exclude = ['api/v1/account/sales'];
+        $referer = parse_url($request->headers->get('referer'));
+
+        if ($request->has('pl') === false and isset($referer['query'])) {
+            parse_str($referer['query'], $query);
+            $request->merge(['pl'=>$query['pl']]);
+        }
         
-        if (! in_array($request->route()->uri, $exclude)) {
-
-            $referer = parse_url($request->headers->get('referer'));
-
-            if ($request->has('pl') === false and isset($referer['query'])) {
-                parse_str($referer['query'], $query);
-                $request->merge(['pl'=>$query['pl']]);
-            }
+        if ($request->has('pl')) {
+            $portalUser = PortalLogin::where('api_token', $request->pl)->first();
             
-            if ($request->has('pl')) {
-                $portalUser = PortalLogin::where('api_token', $request->pl)->first();
-                
-                if ($portalUser) {
+            if ($portalUser) {
+                $exclude = ['api/v1/account/sales'];
+                if (! in_array($request->route()->uri, $exclude)) {
                     if (! $portalUser->can_access_coaching) {
                         $portalUserDetails = json_decode($portalUser->portal_user_details);
                         $portalInstance = explode('\\', $portalUserDetails->instance);
@@ -39,20 +37,20 @@ class AuthenticateInstance
                             'error_message' => 'Unable to load ' . ($portalInstance[2] ?? $portalUserDetails->instance) . ' environment.',
                         ], 200);
                     }
-
-                } else {
-                    abort(response()->json([
-                        'error_code' => 401,
-                        'error_message' => 'Unauthorized',
-                    ], 200));
                 }
 
             } else {
                 abort(response()->json([
-                    'error_code' => 400,
-                    'error_message' => 'Bad Request',
+                    'error_code' => 401,
+                    'error_message' => 'Unauthorized',
                 ], 200));
             }
+
+        } else {
+            abort(response()->json([
+                'error_code' => 400,
+                'error_message' => 'Bad Request',
+            ], 200));
         }
 
         return $next($request);
