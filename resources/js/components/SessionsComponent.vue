9a4b86fcf633319bd75cd2e3d6ec54be108a0313
Jan Israel <jan.israel@smartchartsfx.com>
2022-03-22 00:25:15 +0800

    update

--- resources/js/components/SessionsComponent.vue
+++ resources/js/components/SessionsComponent.vue
@@ -105,7 +105,7 @@
                 <el-avatar :size="60" :src="position.coach_image" class="session-list-avatar">
                   <img :src="position.coach_image" :alt="position.coach_image"/>
                 </el-avatar> 
-                    {{ $moment.tz(new Date(position.date + ' ' + position.start_time), 'Australia/Melbourne').format('HH:mm') }} 
+                    {{ $moment.tz(new Date(position.date + ' ' + position.start_time), 'Australia/Melbourne').utcOffset(0, true).format('HH:mm') }} --
                 <span v-if="tzone.includes(position.sched_timezone)" class="session-list-time">
                   {{ position.start_time }} 
                   {{ $moment.tz(new Date(position.date), tzone).format('dddd') }}
@@ -113,7 +113,7 @@
                   {{ $moment.tz(new Date(position.date), tzone).format('MMM') }}
                 </span>
                 <span v-else class="session-list-time">
-                  {{ $moment.tz(new Date(position.date + ' ' + position.start_time), tzone).format('HH:mm') }} 
+                  {{ $moment.tz(new Date(position.date + ' ' + position.start_time), tzone).utcOffset(0, true).format('HH:mm') }} 
                   {{ $moment.tz(new Date(position.date), tzone).format('dddd') }}
                   {{ $moment.tz(new Date(position.date), tzone).format('Do') }}
                   {{ $moment.tz(new Date(position.date), tzone).format('MMM') }}
