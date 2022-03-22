178904aa8afeab84d0343fdd347ee6bf8d8b37d1
Jan Israel <jan.israel@smartchartsfx.com>
2022-03-22 15:16:53 +0800

    update

--- resources/js/components/SessionsComponent.vue
+++ resources/js/components/SessionsComponent.vue
@@ -535,12 +535,12 @@ export default {
       let date = value.date
       let time = value.start_time
       let date_time = date + ' ' + time
-      let offset = this.$moment.tz(new Date(date_time), tzone).utcOffset()
+      let offset = this.$moment.tz(new Date(date_time), 'Europe/London').utcOffset()
       // let withouttimezone =  this.$moment.tz(new Date(date_time)).utcOffset(0, true).format()
       let res = this.$moment.tz(new Date(date_time), 'Europe/London').format('HH:mm ddd Do MMM')
       let orig = this.$moment.tz(new Date(date_time), coach_tzone).format('HH:mm ddd Do MMM')
       
-      return res;
+      return offset;
     },
     even: function(arr) {
       return arr.slice().sort(function(a, b) {
