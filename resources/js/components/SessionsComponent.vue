3ed14a28fbe7a26b457ce027e2ff17c18d93213a
Jan Israel <jan.israel@smartchartsfx.com>
2022-03-22 17:20:01 +0800

    update

--- resources/js/components/SessionsComponent.vue
+++ resources/js/components/SessionsComponent.vue
@@ -551,16 +551,16 @@ export default {
       let new_off = offset / 60
       // var now = this.$moment.utc();
       // let withouttimezone =  this.$moment.tz(new Date(date_time)).utcOffset(0, true).format()
-      // var Australia_tz_offset = this.$moment.tz(this.coach_tzone).offset(now); 
-      // var London_tz_offset = this.$moment.tz(this.tzone).offset(now);
+      var Australia_tz_offset = this.$moment.tz.zone(this.coach_tzone).offset(now); 
+      var London_tz_offset = this.$moment.tz.zone(this.tzone).offset(now);
 
-      // let diffe = (Australia_tz_offset - London_tz_offset) / 60
-      let converted_offset = this.timeConvert(parseInt(new_off))
+      let diffe = (Australia_tz_offset - London_tz_offset) / 60
+      // let converted_offset = this.timeConvert(parseInt(new_off))
       // let orig = this.$moment.tz(new Date(date_time), 'Australia/Sydney').format('YYYY/MM/DD h:mm')
       // let res = this.$moment.tz(new Date(orig), 'Europe/London').format('h:mm A ddd Do MMM')
 
-      let result = this.$moment(new_date).subtract(offset, 'minutes').format('YYYY/MM/DD h:mm')
-      console.log(offset, 'diff', new_date)
+      let result = this.$moment(date_time).subtract(660, 'minutes').format('YYYY/MM/DD h:mm')
+      console.log(result, 'diff', result)
       return result;
     },
     even: function(arr) {
